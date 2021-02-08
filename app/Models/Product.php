b54dcb0812ea364a9f86dc2405453fb305c4e547
Showman <showman.sh.ahmed@gmail.com>
2021-02-08 15:01:31 +0200

    first commit

---
+++ app/Models/Product.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+   // protected $fillable = ['name','image', 'height', 'width', 'description'];
+    protected $guarded = [];
+    protected $appends = ['image_path'];
+
+    public function getImagePathAttribute()
+    {
+        return asset('uploads/product_images/' . $this->image);
+    }
+
+}
